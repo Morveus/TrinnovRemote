1649a83ba457f5f164e6068767d3c5592042ff3a
Morveus <morveus@morveus.com>
2024-06-09 20:40:41 +0200

    Added MQTT support

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ import os
 import json
 import asyncio
 import websockets
-import flask
+import threading
+import paho.mqtt.client as mqtt
+from paho.mqtt.enums import MQTTProtocolVersion
+import paho.mqtt.publish as publish
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -12,9 +15,43 @@ volume = -60
 muted = False
 dimmed = False
 source_id = 1
+
 amplifier_ip = os.getenv('TRINNOV_AMPLIFIER_IP', '192.168.1.91')
 listen_port = os.getenv('TRINNOV_REMOTE_PORT', '5555')
 websocket_url = f"ws://{amplifier_ip}/ws"
+
+mqtt_broker = os.getenv('MQTT_BROKER', 'localhost')
+mqtt_port = int(os.getenv('MQTT_PORT', '1883'))
+mqtt_name = os.getenv('MQTT_DEVICE_NAME', 'TrinnovAmplitude')
+mqtt_identifier = os.getenv('MQTT_DEVICE_IDENTIFIER', 'trinnov_amp_1')
+mqtt_topic = os.getenv('MQTT_TOPIC', 'trinnovremote/'+mqtt_identifier)
+
+mqtt_client = mqtt.Client()
+
+def on_connect(client, userdata, flags, rc):
+    print(f"Connected with result code {rc}")
+    client.subscribe(f"{mqtt_topic}/#")
+
+def on_message(client, userdata, msg):
+    # Handle the message here based on topic
+    if("/set" in msg.topic):
+        print(f"Received message on topic {msg.topic}: {msg.payload.decode()}")
+
+    if msg.topic == f"{mqtt_topic}/volume/set":
+        volume_set(msg.payload.decode())
+    elif msg.topic == f"{mqtt_topic}/mute/set":
+        set_mute(msg.payload.decode().lower() == 'on')
+    elif msg.topic == f"{mqtt_topic}/dimmed/set":
+        set_dimmed(msg.payload.decode().lower() == 'on')
+    elif msg.topic == f"{mqtt_topic}/source/set":
+        set_source(msg.payload.decode())
+
+def setup_mqtt_client():
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect(mqtt_broker, mqtt_port, 60)
+    client.loop_forever()
 
 async def send_websocket_message(message, reply=False):
     print("Sending this message:")
@@ -113,6 +150,7 @@ def set_volume():
     formatted_volume = format_volume(volume)
     volume_message = build_message(message_byte, b'\x12', '/optimizer/volume/', f'{{"volume":{formatted_volume}}}')
     asyncio.run(send_websocket_message(volume_message))
+    set_mqtt_values()
 
 def set_mute(value):
     global muted
@@ -129,6 +167,7 @@ def set_mute(value):
 
         message = build_message(message_byte, b'\x12', '/optimizer/volume/', f'{{"mute":{message_value}}}')
         asyncio.run(send_websocket_message(message))
+        set_mqtt_values()
     
 def set_dimmed(value):
     global dimmed
@@ -144,6 +183,7 @@ def set_dimmed(value):
 
         message = build_message(message_byte, b'\x12', '/optimizer/volume/', f'{{"dim":{message_value}}}')
         asyncio.run(send_websocket_message(message))
+        set_mqtt_values()
 
 def set_source(id):
     global source_id
@@ -162,6 +202,99 @@ def set_source(id):
     message = build_message(message_byte, b'\x15', '/metapresets/current/', f'{{"id":{id}}}')
     asyncio.run(send_websocket_message(message))
 
+    set_mqtt_values()
+
+def publish_discovery_config():
+    device_config = {
+        "identifiers": [mqtt_identifier],
+        "manufacturer": "Trinnov",
+        "model": "Optimizer",
+        "name": mqtt_name,
+        "sw_version": "1.0"
+    }
+
+    volume_config = {
+        "name": f"Volume",
+        "state_topic": f"{mqtt_topic}/volume/state",
+        "command_topic": f"{mqtt_topic}/volume/set",
+        "unique_id": f"{mqtt_identifier}_volume",
+        "device": device_config,
+        "min": -120,
+        "max": 20, 
+        "step": 1
+    }
+
+    mute_config = {
+        "name": f"Mute",
+        "state_topic": f"{mqtt_topic}/mute/state",
+        "command_topic": f"{mqtt_topic}/mute/set",
+        "unique_id": f"{mqtt_identifier}_mute",
+        "device_class": "switch",
+        "device": device_config
+    }
+
+    dimmed_config = {
+        "name": f"Dimmed",
+        "state_topic": f"{mqtt_topic}/dimmed/state",
+        "command_topic": f"{mqtt_topic}/dimmed/set",
+        "unique_id": f"{mqtt_identifier}_dimmed",
+        "device_class": "switch",
+        "device": device_config
+    }
+
+    source_config = {
+        "name": f"Source",
+        "state_topic": f"{mqtt_topic}/source/state",
+        "command_topic": f"{mqtt_topic}/source/set",
+        "unique_id": f"{mqtt_identifier}_source",
+        "device": device_config,
+        "min": 0,
+        "max": 30, 
+        "step": 1
+    }
+
+    msgs = [
+        {
+            "topic": f"homeassistant/number/{mqtt_identifier}/volume/config",
+            "payload": json.dumps(volume_config),
+            "retain": True
+        },
+        {
+            "topic": f"homeassistant/switch/{mqtt_identifier}/mute/config",
+            "payload": json.dumps(mute_config),
+            "retain": True
+        },
+        {
+            "topic": f"homeassistant/switch/{mqtt_identifier}/dimmed/config",
+            "payload": json.dumps(dimmed_config),
+            "retain": True
+        },
+        {
+            "topic": f"homeassistant/number/{mqtt_identifier}/source/config",
+            "payload": json.dumps(source_config),
+            "retain": True
+        },
+    ]
+
+    # Publish the messages
+    publish.multiple(msgs, hostname=mqtt_broker, port=mqtt_port, protocol=MQTTProtocolVersion.MQTTv5)
+
+def set_mqtt_values():
+    print("Publishing to MQTT topic " + mqtt_topic)
+
+    message_volume = volume
+    message_muted = "ON" if muted else "OFF"
+    message_dimmed = "ON" if dimmed else "OFF"
+    message_source = source_id
+
+    msgs = [
+        {'topic': f"{mqtt_topic}/volume/state", 'payload': str(message_volume)},
+        {'topic': f"{mqtt_topic}/mute/state", 'payload': message_muted},
+        {'topic': f"{mqtt_topic}/dimmed/state", 'payload': message_dimmed},
+        {'topic': f"{mqtt_topic}/source/state", 'payload': str(message_source)}
+    ]
+    publish.multiple(msgs, hostname=mqtt_broker, port=mqtt_port, protocol=MQTTProtocolVersion.MQTTv5)
+
 @app.route('/source/<source>', methods=['GET'])
 def change_source(source):
     set_source(source)
@@ -176,4 +309,12 @@ def index():
 
 if __name__ == '__main__':
     # get_config()
+    publish_discovery_config()
+    set_mqtt_values()
+    
+    # Start the MQTT client in a separate thread
+    mqtt_thread = threading.Thread(target=setup_mqtt_client)
+    mqtt_thread.daemon = True
+    mqtt_thread.start()
+
     app.run(host='0.0.0.0', port=listen_port)
